1d9af9cb076938dd38f0e432bcdcb85461794cbd
Caleb Pardeik <7912201@isd535.org>
2022-04-27 11:32:19 -0500

    p5

---
+++ scripts/ps5.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(600, 600);
+    noStroke()
+}
+
+function draw() {
+    background(0, 243, 80);
+    fill(255, 0, 0);
+    rect(20, 20, 40)
+    fill(0, 0, 255)
+    rect(60, 20, 40)
+}
